638a31e5683e283dd25cb3e6f62ac0ae50709a47
Brett Smith <brettdansmith@gmail.com>
2023-09-18 13:09:31 -0400

    Add fade effect from welcome screen to home screen Change 'home-middle' to 'welcome-middle', home-left etc. Change background image from body to .welcome-screen

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => {
+    const welcomeScreen = document.querySelector('.welcome-screen');
+    const homeScreen = document.querySelector('.home-screen');
+
+    
+        welcomeScreen.style.opacity = '0';
+        homeScreen.style.opacity = '1';
+    
+});
